f82f4dd97b31a9addd040190f902f833684e37fe
06ergin06 <ibrahimh.ergin@gmail.com>
2025-06-23 15:46:11 +0300

    Add router setup and create Home page component

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from "axios"
+
+const posts = ref([]);
+
+onMounted(async() => {
+    const response = await axios.get("http://localhost:8000/posts")
+    posts.value = response.data.posts
+})
+</script>
+<template>
+    <div v-for="post in posts" :key="post">{{ post }}</div>
+</template>
