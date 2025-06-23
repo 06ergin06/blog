c5c2163fbf02d036572f353abd5e359ed4e4bff5
06ergin06 <ibrahimh.ergin@gmail.com>
2025-06-23 15:12:31 +0300

    Add Vue Router setup and integrate with main application

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from '../routes'
 
-createApp(App).mount('#app')
+app = createApp(App)
+app.mount('#app')
+app.use(router)
